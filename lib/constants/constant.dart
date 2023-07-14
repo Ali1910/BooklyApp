013a1a263dbf74c6fa7dd6eb631cfb8e6ea0314c
ali1910 <alihany5559@gmail.com>
2023-07-14 19:06:35 +0300

    create app styles done

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -4,3 +4,4 @@ const kprimaryColor = Color(0xff100B20);
 const KtranstionDuration = Duration(milliseconds: 125);
 
 const kGTSectraFineRegular = 'GT Sectra Fine Regular';
+const KgilroyExtraBold = 'Gilroy-ExtraBold';
