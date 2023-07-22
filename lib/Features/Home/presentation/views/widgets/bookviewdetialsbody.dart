0babc6394e63d892d1c15b219a78ab510fc19b21
ali1910 <alihany5559@gmail.com>
2023-07-22 12:30:55 +0300

    add title and sub title to details

--- lib/Features/Home/presentation/views/widgets/bookviewdetialsbody.dart
+++ lib/Features/Home/presentation/views/widgets/bookviewdetialsbody.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/Core/Utitls/Styles.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/BookRatingItem.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/CustomBookImage.dart';
 import 'package:flutter/material.dart';
+import '../../../../../constants/constant.dart';
 import 'CustomBookDetailsAppbar.dart';
 
 class Bookdetailviewbody extends StatelessWidget {
@@ -14,9 +17,33 @@ class Bookdetailviewbody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppbar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.18),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
             child: const CustomBookItem(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'Atomic Habit',
+            style: styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'James Clear',
+              style: styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          const BookRatingItem()
         ],
       ),
     );
