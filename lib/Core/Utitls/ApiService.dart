06d0d69da2acf763fb0eeaef7cbc402c91d994d1
ali1910 <alihany5559@gmail.com>
2023-08-05 02:11:34 +0300

    impelement fetch newest books

--- lib/Core/Utitls/ApiService.dart
+++ lib/Core/Utitls/ApiService.dart
@@ -3,12 +3,12 @@ import 'package:dio/dio.dart';
 class ApiService {
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
 
-  final Dio dio;
+  final Dio _dio;
 
-  ApiService(this.dio);
+  ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    var reponse = await dio.get('$_baseUrl$endpoint');
+    var reponse = await _dio.get('$_baseUrl$endpoint');
     return reponse.data;
   }
 }
